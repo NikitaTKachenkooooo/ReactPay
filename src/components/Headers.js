417eead3013aef95bcc9cd3edaa83398079847b6
NikitaTKachenkooooo <niketechanel@gmail.com>
2024-08-18 14:30:51 +0300

    fix color button

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -8,6 +8,9 @@ import transferImage from '../components/img/transfer.png';
 import contactImage from '../components/img/contact.png';
 import aboutusImage from '../components/img/aboutus.png';
 import defenseImage from '../components/img/defens.png';
+import LogIn from '../components/img/LogIn.svg';
+import History from '../components/img/History.svg';
+import LogOut from '../components/img/LogOut.svg';
 
 const Header = ({ isAuthenticated, setIsAuthenticated }) => {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -88,26 +91,48 @@ const Header = ({ isAuthenticated, setIsAuthenticated }) => {
           <img src={logo} alt="Company Logo" className="h-25 w-25 mr-2" />
         </div>
 
-        <div className="flex items-center space-x-4">
+        <div className="flex items-center ">
           <button className="text-gray-600 hover:text-gray-900 px-3 py-2 md:hidden" onClick={toggleMenu}>
             <img src={menu} alt="menu" className="h-8 w-8 mr-2" />
           </button>
-          <div className="hidden md:flex items-center space-x-4">
+          <div className="hidden md:flex items-center ">
             {isAuthenticated ? (
               <>
-                <button onClick={NavigateHistory} className="text-gray-600 hover:text-gray-900 px-3 py-2">History</button>
-                <button onClick={handleLogout} className="text-gray-600 hover:text-gray-900 px-3 py-2">Logout</button>
+                <div className="flex items-center font-sans text-16px justify-center bg-gray-100 rounded-lg p-4  hover:bg-purple-950 hover:text-white transition w-[150px] h-[50px] mr-[25px]">
+                  <button className="flex items-center" onClick={NavigateHistory}>
+                    <img src={History} alt="history" className='w-25 h-25 mr-[15.65px]'></img>
+                    История
+                  </button>
+                </div>
+
+                <div className="flex items-center  fonts-sans justify-center bg-gray-100 rounded-lg p-4 hover:bg-purple-950 hover:text-white transition w-[150px] h-[50px] mr-[25px]">
+                  <button className="flex items-center" onClick={handleLogout}>
+                    <img src={LogOut} alt='LogOut' className='h-25 w-25 mr-[15.65px]'></img>
+                    Выйти
+                  </button>
+                </div>
               </>
             ) : (
               <>
-                <button onClick={NavigateRegister} className="text-gray-600 hover:text-gray-900 px-3 py-2">Register</button>
-                <div className="flex-row mr-14">
-                    <button onClick={NavigateLogin} className="text-gray-600 hover:text-gray-900 px-3 py-2">Войти</button>
+                <div className="flex items-center font-sans text-16px justify-center bg-gray-100 rounded-lg p-4  hover:bg-purple-950 hover:text-white transition w-[150px] h-[50px] mr-[25px]">
+                  <button className="flex items-center" onClick={NavigateHistory}>
+                    <img src={History} alt="history" className='w-25 h-25 mr-[15.65px]'></img>
+                    История
+                  </button>
                 </div>
+
+                <div className="flex items-center  fonts-sans justify-center bg-gray-100 rounded-lg p-4 hover:bg-purple-950 hover:text-white transition w-[150px] h-[50px] mr-[25px]">
+                  <button className="flex items-center" onClick={NavigateLogin}>
+                    <img src={LogIn} alt="LogIn" className='h-25 w-25 mr-[15.65px]'></img>
+                    Войти
+                  </button>
+                </div>
+
+
               </>
             )}
-            <button className="text-gray-600 hover:text-gray-900 px-3 py-2" onClick={toggleMenu}>
-              <img src={menu} alt="menu" className="h-8 w-8 mr-2" />
+            <button className="flex items-center justify-center bg-gray-100 hover:bg-purple-950  rounded-lg transition w-[54px]  h-[50px] p-4 " onClick={toggleMenu}>
+              <img src={menu} alt="menu" className="h-22 w-32 " />
             </button>
           </div>
         </div>
