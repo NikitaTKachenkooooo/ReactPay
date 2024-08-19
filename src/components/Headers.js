ae3aa2bcedfcfeb6f57c8b4a4f0bdb9efe4d0409
NikitaTKachenkooooo <niketechanel@gmail.com>
2024-08-20 00:08:43 +0300

    new add

--- src/components/Headers.js
+++ src/components/Headers.js
@@ -3,17 +3,23 @@ import { gsap } from 'gsap';
 import { useNavigate } from 'react-router-dom';
 import logo from './img/logo.jpg';
 import menu from './img/menu.png';
-
+import menuHov from './img/menuHov.png';
 import transferImage from '../components/img/transfer.png';
 import contactImage from '../components/img/contact.png';
 import aboutusImage from '../components/img/aboutus.png';
 import defenseImage from '../components/img/defens.png';
 import LogIn from '../components/img/LogIn.svg';
+import LoginHovEnter from '../components/img/LoginHovEnter.svg';
+import LoginHovLeav from '../components/img/LoginHovLeav.svg';
 import History from '../components/img/History.svg';
+import historyHov from '../components/img/historyHov.svg';
 import LogOut from '../components/img/LogOut.svg';
 
 const Header = ({ isAuthenticated, setIsAuthenticated }) => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [isMenuHovered, setIsMenuHovered] = useState(false);
+  const [isHistoryHovered, setIsHistoryHovered] = useState(false);
+  const [isLoginHovered, setIsLoginHovered] = useState(false);
   const [activeIndex, setActiveIndex] = useState(null);
   const menuRef = useRef(null);
   const blocksRef = useRef([]);
@@ -62,25 +68,33 @@ const Header = ({ isAuthenticated, setIsAuthenticated }) => {
     window.location.href = '/history';
   };
 
-  const handleMouseEnter = (index) => {
-    gsap.to(blocksRef.current[index], {
-      scale: 1.1,
-      boxShadow: '0px 10px 20px rgba(0, 0, 0, 0.2)',
-      duration: 0.3,
-    });
+  const handleMouseEnterMenu = () => {
+    setIsMenuHovered(true);
   };
 
-  const handleMouseLeave = (index) => {
-    gsap.to(blocksRef.current[index], {
-      scale: 1,
-      boxShadow: '0px 0px 0px rgba(0, 0, 0, 0)',
-      duration: 0.3,
-    });
+  const handleMouseLeaveMenu = () => {
+    setIsMenuHovered(false);
+  };
+
+  const handleMouseEnterHistory = () => {
+    setIsHistoryHovered(true);
+  };
+
+  const handleMouseLeaveHistory = () => {
+    setIsHistoryHovered(false);
+  };
+
+  const handleMouseEnterLogin = () => {
+    setIsLoginHovered(true);
+  };
+
+  const handleMouseLeaveLogin = () => {
+    setIsLoginHovered(false);
   };
 
   const handleBlockClick = (path, index) => {
     setActiveIndex(index);
-    setMenuOpen(false); // Close the menu
+    setMenuOpen(false); 
     navigate(path);
   };
 
@@ -88,97 +102,155 @@ const Header = ({ isAuthenticated, setIsAuthenticated }) => {
     <nav className="relative bg-white shadow-md py-4 z-50">
       <div className="container mx-auto flex justify-between items-center px-4">
         <div className="flex items-center">
-          <img src={logo} alt="Company Logo" className="h-25 w-25 mr-2" />
+          <img src={logo} alt="Company Logo" className="h-25 w-25 mr-2 ml-[185px]" />
         </div>
 
-        <div className="flex items-center ">
-          <button className="text-gray-600 hover:text-gray-900 px-3 py-2 md:hidden" onClick={toggleMenu}>
-            <img src={menu} alt="menu" className="h-8 w-8 mr-2" />
-          </button>
-          <div className="hidden md:flex items-center ">
+        <div className="flex items-center">
+          <div className="hidden md:flex items-center">
             {isAuthenticated ? (
               <>
-                <div className="flex items-center font-sans text-16px justify-center bg-gray-100 rounded-lg p-4  hover:bg-purple-950 hover:text-white transition w-[150px] h-[50px] mr-[25px]">
-                  <button className="flex items-center" onClick={NavigateHistory}>
-                    <img src={History} alt="history" className='w-25 h-25 mr-[15.65px]'></img>
-                    История
+                <div
+                  className="flex items-center font-sans text-16px justify-center bg-gray-100 rounded-lg p-4 hover:bg-purple-950 hover:text-white transition w-[150px] h-[50px] mr-[25px]" 
+                  onMouseEnter={handleMouseEnterHistory}
+                  onMouseLeave={handleMouseLeaveHistory}
+                >
+                  <button
+                    className="flex items-center w-full h-full"
+                    onClick={NavigateHistory}
+                  >
+                    <img
+                      src={isHistoryHovered ? historyHov : History}
+                      alt="history"
+                      className="h-5 w-5"
+                    />
+                    <span className="ml-2">История</span>
                   </button>
                 </div>
 
-                <div className="flex items-center  fonts-sans justify-center bg-gray-100 rounded-lg p-4 hover:bg-purple-950 hover:text-white transition w-[150px] h-[50px] mr-[25px]">
-                  <button className="flex items-center" onClick={handleLogout}>
-                    <img src={LogOut} alt='LogOut' className='h-25 w-25 mr-[15.65px]'></img>
-                    Выйти
+                <div
+                  className="flex items-center font-sans justify-center bg-gray-100 rounded-lg p-4 hover:bg-purple-950 hover:text-white transition w-[150px] h-[50px] mr-[25px]"
+                  onMouseEnter={handleMouseEnterLogin}
+                  onMouseLeave={handleMouseLeaveLogin}
+                >
+                  <button
+                    className="flex items-center w-full h-full"
+                    onClick={handleLogout}
+                  >
+                    <img
+                      src={isLoginHovered ?  LoginHovLeav : LogOut }
+                      alt="LogOut"
+                      className="h-5 w-5"
+                    />
+                    <span className="ml-2">Выйти</span>
                   </button>
                 </div>
               </>
             ) : (
               <>
-                <div className="flex items-center font-sans text-16px justify-center bg-gray-100 rounded-lg p-4  hover:bg-purple-950 hover:text-white transition w-[150px] h-[50px] mr-[25px]">
-                  <button className="flex items-center" onClick={NavigateHistory}>
-                    <img src={History} alt="history" className='w-25 h-25 mr-[15.65px]'></img>
-                    История
+                <div
+                  className="flex items-center font-sans text-16px justify-center bg-gray-100 rounded-lg p-4 hover:bg-purple-950 hover:text-white transition w-[150px] h-[50px] mr-[25px]"
+                  onMouseEnter={handleMouseEnterHistory}
+                  onMouseLeave={handleMouseLeaveHistory}
+                >
+                  <button
+                    className="flex items-center w-full h-full"
+                    onClick={NavigateHistory}
+                  >
+                    <img
+                      src={isHistoryHovered ? historyHov : History}
+                      alt="history"
+                      className="h-5 w-5"
+                    />
+                    <span className="ml-2">История</span>
                   </button>
                 </div>
 
-                <div className="flex items-center  fonts-sans justify-center bg-gray-100 rounded-lg p-4 hover:bg-purple-950 hover:text-white transition w-[150px] h-[50px] mr-[25px]">
-                  <button className="flex items-center" onClick={NavigateLogin}>
-                    <img src={LogIn} alt="LogIn" className='h-25 w-25 mr-[15.65px]'></img>
-                    Войти
+                <div
+                  className="flex items-center font-sans justify-center bg-gray-100 rounded-lg p-4 hover:bg-purple-950 hover:text-white transition w-[150px] h-[50px] mr-[25px]"
+                  onMouseEnter={handleMouseEnterLogin}
+                  onMouseLeave={handleMouseLeaveLogin}
+                  onClick={NavigateLogin}
+                >
+                  <button
+                    className="flex items-center w-full h-full"
+                  >
+                    <img
+                      src={isLoginHovered ? LoginHovEnter : LogIn}
+                      alt="LogIn"
+                      className="h-5 w-5"
+                    />
+                    <span className="ml-2">Войти</span>
                   </button>
                 </div>
-
-
               </>
             )}
-            <button className="flex items-center justify-center bg-gray-100 hover:bg-purple-950  rounded-lg transition w-[54px]  h-[50px] p-4 " onClick={toggleMenu}>
-              <img src={menu} alt="menu" className="h-22 w-32 " />
+            <button
+              className="flex items-center justify-center bg-gray-100 hover:bg-purple-950 rounded-lg transition w-[54px] h-[50px] p-4 mr-[185px]"
+              onMouseEnter={handleMouseEnterMenu}
+              onMouseLeave={handleMouseLeaveMenu}
+              onClick={toggleMenu}
+            >
+              <img
+                src={isMenuHovered ? menuHov : menu}
+                alt="menu"
+                className="h-6 w-6"
+              />
             </button>
           </div>
         </div>
       </div>
+      
 
       <div
-        ref={menuRef}
-        className="absolute left-0 top-full w-full bg-white shadow-md overflow-hidden z-50"
-        style={{ height: 0 }}
+  ref={menuRef}
+  className="absolute left-0 top-full w-full bg-white shadow-md overflow-hidden z-50"
+  style={{ height: 0 }}
+>
+  <div
+    className="flex flex-col md:flex-row justify-between items-center mb-4 mt-4"
+    style={{ 
+      padding: '0 223.5px', // Отступы слева и справа для контейнера
+      gap: '103px' // Отступ между кнопками
+    }}
+  >
+    {[{
+      img: transferImage,
+      alt: "Translation",
+      text: "Переводы",
+      path: "/transfer",
+    }, {
+      img: aboutusImage,
+      alt: "About Us",
+      text: "О нас",
+      path: "/abouts",
+    }, {
+      img: defenseImage,
+      alt: "Protection",
+      text: "Защита данных",
+      path: "/securityinfo",
+    }, {
+      img: contactImage,
+      alt: "Contacts",
+      text: "Контакты",
+      path: "/contacts",
+    }].map((block, index) => (
+      <div
+        key={index}
+        ref={(el) => blocksRef.current[index] = el}
+        className={`flex rounded-lg flex-col items-center justify-center p-4 w-full md:w-[190px] h-[50px] cursor-pointer transition-transform duration-300 hover:bg-purple-950 hover:text-white bg-gray-100 ${activeIndex === index ? 'bg-gray-200' : 'bg-gray-100'}`}
+        onClick={() => handleBlockClick(block.path, index)}
       >
-        <div className="flex flex-col md:flex-row justify-center items-center mb-4 mt-4 gap-4">
-          {[{
-            img: transferImage,
-            alt: "Translation",
-            text: "Переводы",
-            path: "/transfer",
-          }, {
-            img: aboutusImage,
-            alt: "About Us",
-            text: "О нас",
-            path: "/abouts",
-          }, {
-            img: defenseImage,
-            alt: "Protection",
-            text: "Защита данных",
-            path: "/securityinfo",
-          }, {
-            img: contactImage,
-            alt: "Contacts",
-            text: "Контакты",
-            path: "/contacts",
-          }].map((block, index) => (
-            <div
-              key={index}
-              ref={(el) => blocksRef.current[index] = el}
-              className={`flex rounded-[25px] flex-col items-center justify-start p-2 mx-8 w-[66%] md:w-[190px] h-[196px] cursor-pointer transition-transform duration-300 mb-4 ${activeIndex === index ? 'bg-grayth' : ''}`}
-              onMouseEnter={() => handleMouseEnter(index)}
-              onMouseLeave={() => handleMouseLeave(index)}
-              onClick={() => handleBlockClick(block.path, index)}
-            >
-              <span className="mt-4 mb-10 font-bold">{block.text}</span>
-              <img src={block.img} alt={block.alt} className="h-20 w-20" />
-            </div>
-          ))}
+        <div className="flex items-center">
+          <img src={block.img} alt={block.alt} className="h-5 w-5" />
+          <span className="ml-2 font-sans text-16px">{block.text}</span>
         </div>
       </div>
+    ))}
+  </div>
+</div>
+
+
+
     </nav>
   );
 };
